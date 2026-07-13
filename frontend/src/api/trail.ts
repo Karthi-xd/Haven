86651fbca7890ff51378a42887c59f4544c5a1a2
Karthi <dhilipkarthi03@gmail.com>
2026-07-13 18:51:23 +0530

    error fix

--- frontend/src/api/trail.ts
+++
@@ -1,6 +0,0 @@
-import { supabase } from "../lib/supabaseClient";
-import type { TrailNode } from "../types";
-
-export async function fetchTrail(authorId: string): Promise<TrailNode[]> {
-  return [];
-}
